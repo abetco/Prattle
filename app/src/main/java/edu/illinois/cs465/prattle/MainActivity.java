96b98c96e41ff9853f6c9c67f5f6c6205e577d01
Diana <Dianagolmeeva@gmail.com>
2021-04-14 17:18:34 -0500

    Implemented google play services and calendar into gradle. Started messing with calendarview

--- app/src/main/java/edu/illinois/cs465/prattle/MainActivity.java
+++ app/src/main/java/edu/illinois/cs465/prattle/MainActivity.java
@@ -17,12 +17,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        Button button = findViewById(R.id.calendar);//feel free to move/get rid of this button
-//        button.setOnClickListener(view -> {
-//            Intent intent = new Intent(this,UpdateCalendar.class);
-//            startActivity(intent);
-//        });
-
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
         bottomNavigationView.setOnNavigationItemSelectedListener(navListener);
     }
