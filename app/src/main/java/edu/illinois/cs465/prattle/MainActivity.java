a267d780ed4bd5548a451c1e3a164a0ecad839fd
Albert Cao <albertc4@illinois.edu>
2021-04-13 21:18:06 -0500

    Fixed navigation to calendar activity

--- app/src/main/java/edu/illinois/cs465/prattle/MainActivity.java
+++ app/src/main/java/edu/illinois/cs465/prattle/MainActivity.java
@@ -1,9 +1,11 @@
 package edu.illinois.cs465.prattle;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -15,24 +17,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button button = findViewById(R.id.page_1);//feel free to move/get rid of this button
-        button.setOnClickListener(view -> {
-            Intent intent = new Intent(this,UpdateCalendar.class);
-            startActivity(intent);
-        });
-//        BottomNavigationView.OnNavigationItemSelectedListener { item ->
-//                when(item.itemId) {
-//            R.id.item1 -> {
-//                // Respond to navigation item 1 click
-//                true
-//            }
-//            R.id.item2 -> {
-//                // Respond to navigation item 2 click
-//                true
-//            }
-//        else -> false
-//        }
-//        }
+//        Button button = findViewById(R.id.calendar);//feel free to move/get rid of this button
+//        button.setOnClickListener(view -> {
+//            Intent intent = new Intent(this,UpdateCalendar.class);
+//            startActivity(intent);
+//        });
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(navListener);
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    switch (item.getItemId()) {
+                        case R.id.nav_calendar:
+                            Intent intent = new Intent(MainActivity.this, UpdateCalendar.class);
+                            startActivity(intent);
+                            break;
+                        case R.id.nav_plan_hangout:
+                            break;
+                        case R.id.nav_contacts:
+                            break;
+                        default:
+                            throw new IllegalStateException("Unexpected value: " + item.getItemId());
+                    }
+                    return false;
+                }
+            };
 }
 
