c181834dd819e4440c4dd8f8271355f78af36dbf
Albert Cao <albertc4@illinois.edu>
2021-04-14 21:29:39 -0500

    Added contact activity

--- app/src/main/java/edu/illinois/cs465/prattle/MainActivity.java
+++ app/src/main/java/edu/illinois/cs465/prattle/MainActivity.java
@@ -10,6 +10,7 @@ import android.view.View;
 import android.widget.Button;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,8 +18,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
         bottomNavigationView.setOnNavigationItemSelectedListener(navListener);
+
+//        TabLayout tabLayout = findViewById(R.id.tab_layout);
+//        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener{
+//            override fun onTabSelected(tab: TabLayout.Tab?) {
+//                // Handle tab select
+//            }
+//
+//            override fun onTabReselected(tab: TabLayout.Tab?) {
+//                // Handle tab reselect
+//            }
+//
+//            override fun onTabUnselected(tab: TabLayout.Tab?) {
+//                // Handle tab unselect
+//            }
+//        });
     }
 
     private BottomNavigationView.OnNavigationItemSelectedListener navListener =
@@ -27,12 +44,14 @@ public class MainActivity extends AppCompatActivity {
                 public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                     switch (item.getItemId()) {
                         case R.id.nav_calendar:
-                            Intent intent = new Intent(MainActivity.this, UpdateCalendar.class);
-                            startActivity(intent);
+                            Intent calIntent = new Intent(MainActivity.this, UpdateCalendar.class);
+                            startActivity(calIntent);
                             break;
                         case R.id.nav_plan_hangout:
                             break;
                         case R.id.nav_contacts:
+                            Intent conIntent = new Intent(MainActivity.this, UpdateContacts.class);
+                            startActivity(conIntent);
                             break;
                         default:
                             throw new IllegalStateException("Unexpected value: " + item.getItemId());
---
+++ app/src/main/java/edu/illinois/cs465/prattle/UpdateContacts.java
@@ -0,0 +1,14 @@
+package edu.illinois.cs465.prattle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UpdateContacts extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_contacts);
+    }
+}
