f05575cd1b12c8c0125c7a640c78037a329c397d
Diana <Dianagolmeeva@gmail.com>
2021-04-29 19:46:52 -0500

    no title for tool bar

--- app/src/main/java/edu/illinois/cs465/prattle/UpdateCalendar.java
+++ app/src/main/java/edu/illinois/cs465/prattle/UpdateCalendar.java
@@ -29,6 +29,7 @@ public class UpdateCalendar extends AppCompatActivity {
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_update_calendar);
+            getSupportActionBar().setTitle("");
 
             week_days = getResources().getStringArray(R.array.weekdays);
 
